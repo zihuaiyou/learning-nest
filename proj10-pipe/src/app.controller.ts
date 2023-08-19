452857388079005e3b9efc390357516981c7338f
zihuaiyou <zihuaiyou1996@qq.com>
2023-08-20 00:14:02 +0800

    管道init

--- proj10-pipe/src/app.controller.ts
+++ proj10-pipe/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client'; //nest 自带的管道
+import { HandleIntPipe } from './handleInt/handleInt.pipe';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  prisma:PrismaClient
+  constructor(private readonly appService: AppService) {
+    this.prisma = new PrismaClient()
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello(@Param('id',HandleIntPipe) id:number) {
+    // return this.appService.getHello();
+    console.log(typeof id);
+    
+    return this.prisma.article.findUnique({
+      where:{
+        id:id,
+      }
+    })
   }
 }
---
+++ proj10-pipe/src/handleInt/handleInt.pipe.ts
@@ -0,0 +1,10 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class HandleIntPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    // throw new BadRequestException('参数错误')
+    console.log(metadata);
+    return metadata.metatype === Number ? +value : value
+  }
+}
