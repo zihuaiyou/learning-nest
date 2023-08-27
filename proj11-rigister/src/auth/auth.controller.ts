fd870df54e3fd5639933032708fdcac04f3f3e08
zihuaiyou <zihuaiyou1996@qq.com>
2023-08-27 13:00:55 +0800

    注册用户 引入prisma

--- proj11-rigister/src/auth/auth.controller.ts
+++ proj11-rigister/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import RigisterDto from './dto/rigister.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+    @Post("register")
+    register(@Body() dto:RigisterDto) {
+       return this.authService.register(dto)
+    }
+}
--- proj11-rigister/src/auth/auth.service.ts
+++ proj11-rigister/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import RigisterDto from './dto/rigister.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly prisma: PrismaService) { }
+    async register(dto: RigisterDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: 1
+            }
+        })
+        return user
+    }
+}
