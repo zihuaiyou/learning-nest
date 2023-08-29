71e3aae48bae0e4111bd162df98d81b50e0214c6
zihuaiyou <zihyou@yonyou.com>
2023-08-29 10:30:32 +0800

    jwt 注册功能实现

--- proj12-jwt/src/auth/auth.controller.ts
+++ proj12-jwt/src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import AuthDto from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
     @Post('register')
-    rigister(@Body() body: any) {
-        return 'jwt初始化'
+    rigister(@Body() dto: AuthDto) {
+        return this.authService.regisger(dto)
     }
 }
--- proj12-jwt/src/auth/auth.service.ts
+++ proj12-jwt/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import AuthDto from './dto/auth.dto';
+import { hash } from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly prisma: PrismaService) { }
+    async regisger(dto: AuthDto) {
+        const password = await hash(dto.password, 8)
+        const user = await this.prisma.user.create({
+            data: {
+                name: dto.name,
+                password
+            }
+        })
+        console.log(user);
+        return user
+    }
+}
