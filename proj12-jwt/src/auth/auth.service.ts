43d33a109b8c529cba554151f7ebcee34af42ee9
zihuaiyou <zihuaiyou1996@qq.com>
2023-08-31 00:22:54 +0800

    生成token

--- proj12-jwt/src/auth/auth.service.ts
+++ proj12-jwt/src/auth/auth.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import AuthDto from './dto/auth.dto';
 import { hash } from 'bcryptjs';
+import { user } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) { }
+    constructor(private readonly prisma: PrismaService,private readonly jwt:JwtService) { }
     async regisger(dto: AuthDto) {
         const password = await hash(dto.password, 8)
         const user = await this.prisma.user.create({
@@ -14,7 +16,14 @@ export class AuthService {
                 password
             }
         })
-        console.log(user);
-        return user
+        return this.token(user)
+    }
+    async token({name,id}:user){
+        return {
+            token:await this.jwt.signAsync({
+                name,
+                sub:id
+            })
+        }
     }
 }
--- proj12-jwt/src/prisma/prisma.service.ts
+++ proj12-jwt/src/prisma/prisma.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient{}
+export class PrismaService extends PrismaClient{
+    constructor(){
+        super(process.env.NODE_ENV === 'development'?{log:['query']}:{})
+    }
+}
