021c84dac5caa3783fa596a37a7e1d652a764ea6
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-09 17:48:31 +0800

    文件类型校验

--- proj13-upload/src/upload/upload.controller.ts
+++ proj13-upload/src/upload/upload.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, MethodNotAllowedException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
-  constructor() {}
+  constructor() { }
   @Post('image')
-  @UseInterceptors(FileInterceptor('file'))
-    upload(@UploadedFile() file:Express.Multer.File){
-        return file.originalname
+  @UseInterceptors(FileInterceptor('file', {
+    limits: { fileSize: Math.pow(1024, 2) },
+    fileFilter(req: any, file: Express.Multer.File, callback: (error: Error | null, acceptFile: boolean) => void) {
+      if (file.mimetype.includes('image')) {
+        callback(null, true)
+      } else {
+        callback(new MethodNotAllowedException('文件类型不允许'), false)
+      }
     }
+  }
+  )
+  )
+  upload(@UploadedFile() file: Express.Multer.File) {
+    // console.log(file.mimetype);
+    // image/jpeg
+    return file
+  }
 }
