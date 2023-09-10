e73174e67a854f5e2d0a399d4b51273f44bcc2e0
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-10 17:55:53 +0800

    通过url访问文件

--- proj13-upload/src/main.ts
+++ proj13-upload/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptors/transform-interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new TransformInterceptor())
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('uploads', { prefix: '/uploads' });
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
