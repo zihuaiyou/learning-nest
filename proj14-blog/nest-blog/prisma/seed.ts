199d69138357dd1f30c52943fdcfbab59a9a2a93
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-16 16:13:34 +0800

    nest-blog init

---
+++ proj14-blog/nest-blog/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'argon2';
+import { Random } from 'mockjs';
+
+const prisma = new PrismaClient();
+async function run() {
+  await prisma.user.create({
+    data: {
+      name: 'zihyou1996',
+      password: await hash('19960808'),
+    },
+  });
+
+  for (let index = 0; index < 10; index++) {
+    await prisma.article.create({
+      data: {
+        title: Random.ctitle(5, 10),
+        content: Random.cparagraph(30, 50),
+      },
+    });
+  }
+}
+
+run();
