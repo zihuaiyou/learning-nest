8e04d8395957cfd72177601083724a977e557bb5
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-16 17:58:04 +0800

    注册验证2

--- proj14-blog/nest-blog/src/common/validate.ts
+++ proj14-blog/nest-blog/src/common/validate.ts
@@ -1,3 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
-export class Validate extends ValidationPipe {}
+export class Validate extends ValidationPipe {
+  protected flattenValidationErrors(
+    validationErrors: ValidationError[],
+  ): string[] {
+    const messages = {};
+    validationErrors.forEach((error) => {
+      messages[error.property] = Object.values(error.constraints)[0];
+    });
+    throw new HttpException({ code: 400, messages }, HttpStatus.BAD_REQUEST);
+  }
+}
