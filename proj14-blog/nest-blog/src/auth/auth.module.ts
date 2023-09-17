fe0352c3b8d9b013b159417261063ecfe34882bf
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-17 23:47:10 +0800

    校验用户，加token

--- proj14-blog/nest-blog/src/auth/auth.module.ts
+++ proj14-blog/nest-blog/src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: function (config: ConfigService) {
+        return {
+          secret: config.get('TOKEN_SECRET'),
+          signOptions: { expiresIn: '100d' },
+        };
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- proj14-blog/nest-blog/src/auth/auth.service.ts
+++ proj14-blog/nest-blog/src/auth/auth.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import RegisterDto from './dto/register.dto';
 import { hash } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
   async register(dto: RegisterDto) {
-    return await this.prisma.user.create({
+    const user = await this.prisma.user.create({
       data: {
         name: dto.name,
         password: await hash(dto.password),
       },
     });
+    return this.token(user);
+  }
+  private async token({ id, name }) {
+    return await this.jwt.signAsync({ sub: id, name });
   }
 }
