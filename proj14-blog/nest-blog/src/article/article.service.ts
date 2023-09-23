3ef2f3689fafa8e63b1ee80932c27cc3c3a4c37d
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-23 23:40:57 +0800

    查找全部文章

--- proj14-blog/nest-blog/src/article/article.service.ts
+++ proj14-blog/nest-blog/src/article/article.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArticleService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createArticleDto: CreateArticleDto) {
     return 'This action adds a new article';
   }
 
-  findAll() {
-    return `This action returns all article`;
+  async findAll() {
+    // return `This action returns all article`;
+    const articles = await this.prisma.article.findMany();
+    return articles;
   }
 
   findOne(id: number) {
