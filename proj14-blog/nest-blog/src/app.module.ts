dcf0bd051e3f4ad20799d2668f688f804c5886e8
zihuaiyou <zihuaiyou1996@qq.com>
2023-09-23 23:29:22 +0800

    文章模块的增删改查创建

--- proj14-blog/nest-blog/src/app.module.ts
+++ proj14-blog/nest-blog/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, ArticleModule],
 })
 export class AppModule {}
