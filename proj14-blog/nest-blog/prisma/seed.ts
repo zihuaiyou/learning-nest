acf061e10a0e3712140f129f6d288610c0d20129
zihuaiyou <zihuaiyou1996@qq.com>
2023-10-17 23:14:54 +0800

    文章分类 表的数据迁移

--- proj14-blog/nest-blog/prisma/seed.ts
+++ proj14-blog/nest-blog/prisma/seed.ts
@@ -11,11 +11,20 @@ async function run() {
     },
   });
 
+  for (let index = 1; index < 6; index++) {
+    await prisma.category.create({
+      data: {
+        title: Random.ctitle(3, 6),
+      },
+    });
+  }
+
   for (let index = 0; index < 50; index++) {
     await prisma.article.create({
       data: {
         title: Random.ctitle(5, 10),
         content: Random.cparagraph(30, 50),
+        categoryId: Random.integer(1,5)
       },
     });
   }
